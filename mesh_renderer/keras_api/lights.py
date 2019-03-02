71eb6cd3a507351e1eeeb418604b0ad6c1fcd8f5
Jason Jennings <jason.jennings@mavs.uta.edu>
2019-03-02 01:11:16 -0600

    Began creating keras interface

--- mesh_renderer/keras_api/lights.py
+++ mesh_renderer/keras_api/lights.py
@@ -19,7 +19,7 @@ class Lights(object):
             self.positions = K.variable(positions, name='light_positions')
         else:
             raise ValueError("type must be fixed or variable")
-    elif isinstance(positions, tf.Tensor):
+    elif isinstance(positions, (tf.Tensor, tf.Variable)):
         self.positions = positions
     else:
         raise ValueError("positions must be numpy array or tensor")
@@ -31,7 +31,7 @@ class Lights(object):
             self.intensities = K.variable(positions, name='light_intensities')
         else:
             raise ValueError("type must be fixed or variable")
-    elif isinstance(intensities, tf.Tensor):
+    elif isinstance(intensities, (tf.Tensor, tf.Variable)):
         self.intensities = intensities
     else:
         raise ValueError("positions must be numpy array or tensor")
