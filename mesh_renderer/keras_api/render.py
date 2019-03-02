9921d376fbad40d2ca27250aab9aa8f192743fef
Jason Jennings <jason.jennings@mavs.uta.edu>
2019-03-02 02:11:23 -0600

    Began creating keras interface

--- mesh_renderer/keras_api/render.py
+++ mesh_renderer/keras_api/render.py
@@ -9,25 +9,24 @@ class Render(keras.layers.Layer):
         super(Render, self).build(input_shape)
 
     def call(self, x):
-        vertices, faces = x[0]
-        normals = x[1]
-        colors = x[2]
-        eye, center, world_up = x[3]
-        light_positions, light_intensities = x[4]
-
+        vertices, faces = x[0], x[1]
+        normals = x[2]
+        colors = x[3]
+        eye, center, world_up = x[4], x[5], x[6]
+        light_positions, light_intensities = x[7], x[8]
+        print(K.tile(light_intensities[tf.newaxis, :, :], [vertices.shape[0], 1, 1]))
         return mesh_renderer.mesh_renderer(vertices,
                                            faces,
                                            normals,
                                            colors,
-                                           K.tile(eye[tf.newaxis, :, :], [vertices.shape[0], 1, 1]),
-                                           K.tile(center[tf.newaxis, :, :], [vertices.shape[0], 1, 1]),
-                                           K.tile(world_up[tf.newaxis, :, :], [vertices.shape[0], 1, 1]),
+                                           K.tile(eye[tf.newaxis, :], [vertices.shape[0], 1]),
+                                           K.tile(center[tf.newaxis, :], [vertices.shape[0], 1]),
+                                           K.tile(world_up[tf.newaxis, :], [vertices.shape[0], 1]),
                                            K.tile(light_positions[tf.newaxis, :, :], [vertices.shape[0], 1, 1]),
                                            K.tile(light_intensities[tf.newaxis, :, :], [vertices.shape[0], 1, 1]),
                                            self.resolution[0],
                                            self.resolution[1])
 
+
     def get_output_shape_for(self, input_shape):
         return (input.shape[0], self.resolution[0], self.resolution[1])
-
-Renderer()([world_coords, base_model.faces], base_model.calculate_normals(woorld_coords), colors, [camera.eye, camera.center, camera.world_up], [lights.positions, lights.intensities])
--- mesh_renderer/keras_api/transform.py
+++ mesh_renderer/keras_api/transform.py
@@ -1,22 +1,19 @@
 import keras
 from keras import backend as K
 import tensorflow as tf
-
+import numpy as np
 
 class Transform(keras.layers.Layer):
     def __init__(self, batch_size, **kwargs):
         self.batch_size = batch_size
         super(Transform, self).__init__(**kwargs)
-​
+
     def build(self, input_shape):
-        self.matrix = self.add_weight(name='matrix',
-                                         shape=(self.batch_size, 3, 3),
-                                         initializer='uniform',
-                                         trainable=True)
-        super(Transform, self).build(input_shape)
-​
+      self.matrix = K.variable(np.random.randn(self.batch_size, 3, 3))
+      super(Transform, self).build(input_shape)
+
     def call(self, x):
         return tf.matmul(x, self.matrix, transpose_b=True)
-​
+
     def get_output_shape_for(self, input_shape):
         return input_shape
