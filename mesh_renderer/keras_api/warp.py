7940e0cda44c9ce60ccd6749056ce0ed258d03da
Jason Jennings <jason.jennings@mavs.uta.edu>
2019-03-02 00:00:53 -0600

    Began creating keras interface

--- mesh_renderer/keras_api/warp.py
+++ mesh_renderer/keras_api/warp.py
@@ -31,7 +31,7 @@ class Warp(keras.layers.Layer):
 
     def call(self, x):
         vertices, warp_params = x[0], x[1]
-        print([x[1].shape[0], 1, 1])
+        print(vertices, warp_params)
         vertices_repeated = tf.tile(vertices[tf.newaxis, :, :], [tf.shape(x[1])[0], 1, 1])
         warped_vertices = tf.foldl(warp_rbf,
                                    elems=[self.keypoints, self.warp_vectors,
